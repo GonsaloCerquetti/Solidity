533c3839cf13b338932955cb3932c93da2d9776a
Gonsalo <gonsalocerquetti97@gmail.com>
2018-10-10 17:19:19 -0300

    mapping

--- Tutoria.sol
+++ Tutoria.sol
@@ -1,38 +1,43 @@
 pragma solidity ^0.4.7;
 contract Tutoria {
     
-    string materia;
-    address profesor;
-    address alumno;
-    uint confi = 0;
-    
-    function pedir (string mat, address prof) public{
-        materia = mat;
-        alumno = msg.sender;
-        require(msg.sender != prof);
-        profesor = prof;
-       
-        
+    struct tutoriaData{
+        string materia;
+        address profesor;
+        address alumno;
+        bool confir;
     }
     
+    function pedir (string mat, address prof) public{
+        tutoriaData.materia = mat;
+        tutoriaData.alumno = msg.sender;
+        require(msg.sender != prof);
+        tutoriaData.profesor = prof;
+     }
+    
+    mapping (address => tutoriaData) tutorias;
+    
     function getMateria() public returns (string) {
-        return materia;
+        return tutoriaData.materia;
     }
     
     function getProfesor() public returns (address) {
-        return profesor;
+        return tutoriaData.profesor;
     }
     
     function getAlumno() public returns (address) {
-        return alumno;
+        return tutoriaData.alumno;
     }
     
     function confirmar() public returns (uint){
-        return confi=1;
+        tutoriaData.confir = true;
     }
     
     function confirmado() public returns (uint){
-        require(confi==1);
+        require(tutoriaData.confir == true);
+        return tutoriaData.confir;
+    }
+}
         return confi;
     }
 }
